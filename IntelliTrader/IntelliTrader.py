f7d863f986a208d03fa375f2edc6d3524aa726d7
Srikrishnan P <krishnan.sri92@gmail.com>
2023-06-15 09:18:50 +0530

    Fix the driver not found, stale element error during runtime with a implicit wait time #25

--- IntelliTrader/IntelliTrader.py
+++ IntelliTrader/IntelliTrader.py
@@ -9,14 +9,6 @@ config = open(user_cred_path,'r').read().split()
 
 # Begin connection
 c = Connection(config)
-kite = c.broker_login(KiteConnect)
-
-instrument_dump = kite.instruments('NSE')
-instrument_df = pd.DataFrame(instrument_dump)
-
-if os.path.exists('./output/'):
-    instrument_df.to_csv('./output/NSE_Instruments.csv', index=False)
-else :
-    os.mkdir('./output/') 
-    instrument_df.to_csv('./output/NSE_Instruments.csv', index=False) 
+kite, request_token, access_token = c.broker_login(KiteConnect)
+kite.set_access_token(access_token)
 
--- IntelliTrader/connection.py
+++ IntelliTrader/connection.py
@@ -30,7 +30,7 @@ class Connection:
 
         # auto enter login information
         driver.get(kite.login_url())
-        driver.implicitly_wait(1)
+        driver.implicitly_wait(5)
 
         # username input
         username = WebDriverWait(driver, 10).until(
@@ -42,12 +42,12 @@ class Connection:
             lambda x: x.find_element(by=By.XPATH, value='//*[@id="password"]'))
         password.send_keys(user_pass)
 
+        driver.implicitly_wait(5)
+
         # submit button
         submit = WebDriverWait(driver, 10).until(
             lambda x: x.find_element(by=By.XPATH, value='//*[@id="container"]/div/div/div[2]/form/div[4]/button'))
         submit.click()
-
-        driver.implicitly_wait(1)
 
         # mfa / external TOTP 
         totp = WebDriverWait(driver, 10).until(
@@ -55,19 +55,35 @@ class Connection:
         authkey = pyotp.TOTP(mfa_token)
         totp.send_keys(authkey.now())
 
-        # continue button
-        continue_btn = WebDriverWait(driver, 100).until(
-            lambda x: x.find_element(by=By.XPATH, value='//*[@id="container"]/div[2]/div/div/form/div[2]/button'))
-        continue_btn.click()
+        driver.implicitly_wait(5)
 
+        def find(driver):
+            element = driver.find_element(by=By.XPATH, value='//*[@id="container"]/div[2]/div/div/form/div[2]/button')
+            if element:
+                return element
+            else:
+                return False
+
+        # continue button
+        continue_btn = WebDriverWait(driver, 10).until(find)
+        if(continue_btn):
+            continue_btn.click()
+
+        # request token generation
         url = driver.current_url
         initial_token = url.split('request_token=')[1]
         request_token = initial_token.split('&')[0]
         with open('./output/request_token.txt', 'w') as r_file:
             r_file.write(request_token)
 
+        # access token generation
+        data = kite.generate_session(request_token, api_secret=secret_key)
+        access_token = data['access_token']
+        with open('./output/access_token.txt', 'w') as a_file:
+            a_file.write(access_token)
+
         driver.close()
 
-        data = kite.generate_session(request_token, api_secret=secret_key)
-        kite.set_access_token(data['access_token'])
-        return kite
+        return kite, request_token, access_token
+
+
